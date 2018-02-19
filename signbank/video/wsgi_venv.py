77feedecbd67c4eda920593fee1b3af4bdae9b53
Per Låås <per.laas@sprakochfolkminnen.se>
2018-02-19 16:19:27 +0100

    OS-276 Installera FinSL-signbank för testning i testserver. Test wsgi mot venv.

---
+++ signbank/video/wsgi_venv.py
@@ -0,0 +1,29 @@
+"""
+exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+import os
+import time
+import traceback
+import signal
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+# sys.path.append('/var/www/vhosts/mysite')
+sys.path.append('/var/www/django/teckenlistor/venv')
+# adjust the Python version in the line below as needed
+# sys.path.append('/var/www/vhosts/mysite/venv/lib/python3.5/site-packages')
+sys.path.append('/var/www/django/teckenlistor/venv/python/site-packages')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "signbank.settings.production")
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
