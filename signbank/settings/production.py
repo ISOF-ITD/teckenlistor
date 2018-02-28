f0fce53277e58de0268b799e9b73f0359dcb3bb8
Per Låås <per.laas@sprakochfolkminnen.se>
2018-02-28 14:06:22 +0100

    OS-294 Temporärt fixa länkar i templates för användare i huvudsak ska testa. Korrigerat media paths.

--- signbank/settings/production.py
+++ signbank/settings/production.py
@@ -31,7 +31,7 @@ STATICFILES_DIRS = (
 )
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/var/www/django/signbank/media/'
+MEDIA_ROOT = '/var/www/django/media/'
 # MEDIA_ROOT = '/var/www/signbank/media/'
 # URL that handles the media served from MEDIA_ROOT, used for managing stored files.
 # It must end in a slash if set to a non-empty value.
